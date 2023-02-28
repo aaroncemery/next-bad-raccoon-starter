98e18c4f2fc660b24203945e3ce427945a097e66
Aaron Emery <aemery81@gmail.com>
2023-02-27 19:45:43 -0800

    test: testing pre-push hook

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <RootLayout>
       <main>
         <h1 className="container font-sans text-5xl text-blue-50">Welcome</h1>
+        <h1 className="container font-sans text-5xl text-blue-50">Test</h1>
       </main>
     </RootLayout>
   )
